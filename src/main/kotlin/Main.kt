026227833c16ff0f34fdc8b58263668adf014223
Swamy Sivasubramaniyan <sswamyn@users.noreply.github.com>
2023-09-29 20:57:13 -0600

    Redesign :)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,9 +4,11 @@ import Data.aWeekScheduleData
 import java.io.File
 import mu.KotlinLogging
 
-var currentLine = 0
-var excelSheet = mutableListOf<String>()
-val fileNameStr = "src/main/resources/python/TrainingScheduleDefinitionscopy.csv"
+//var currentLine = 0
+var excelSheet = mutableListOf<ExcelRowData>()
+var idxOfWeeks = mutableListOf<ExcelRowData>()
+
+val fileNameStr = "src/main/resources/python/Training Schedule Definitions.csv"
 private val logger = KotlinLogging.logger {}
 
 fun main(args: Array<String>) {
@@ -14,117 +16,42 @@ fun main(args: Array<String>) {
     // Add logging to the application
     logger.info { "Program started" }
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
-
     // Call readFile()  to read the file and popuplate the mutable ArrayList
     readFile()
 
     //var fullScheduleData is the main output in the form of a Java object;
     //    Next Step(Final): Convert this to a JSON object
     var fullScheduleData: Array<aWeekScheduleData> = emptyArray()
+    var weekIdx: Int = 1
+    do {
+        if (weekIdx <= idxOfWeeks.size) {
+            //val rowsForThisWeek: Array<ExcelRowData> = excelSheet[idxOfWeeks[weekIdx] .. idxOfWeeks[(weekIdx + 1)]].toTypedArray()
+            logger.debug { " weekIdx: $weekIdx \t weekIdx+1: ${weekIdx + 1} \t \t idxOfWeeks.size: ${idxOfWeeks.size} \t idxOfWeeks[weekIdx]: ${idxOfWeeks[weekIdx]}" }
+            val rowsForThisWeek: Array<ExcelRowData> = excelSheet.subList(weekIdx, weekIdx+1).toTypedArray()
+            logger.debug { " rowsForThisWeek.size: ${rowsForThisWeek.size} \t $weekIdx : ${weekIdx+1}" }
 
-    // read file line by line
-    //file.forEachLine {
-    //for (aLine in excelSheet) {
-    val excelSheetIterator = excelSheet.listIterator()
-    for ((index, aLine) in excelSheetIterator.withIndex()) {
-        currentLine++
-        logger.debug { "CurrentLine is: $currentLine \t Index: $index \t\t Value is: $aLine" }
+        }
+        weekIdx++
+    } while (weekIdx < idxOfWeeks.size )
 
-        logger.info { "Read the first line; \t Current line: $currentLine" }
-        val oneExcelRow: ExcelRowData = createExcelRowData(aLine)
-        logger.debug { " Index of list (excelSheet) is $index " }
-        logger.info { "Got the first row tokenized \t oneExcelRow: $oneExcelRow" }
-        val rowsForThisWeek: Array<ExcelRowData> = getExcelRowDataForThisWeek(oneExcelRow.weekToken, excelSheetIterator)
 
-        var oneWeek: aWeekScheduleData = aWeekScheduleData(oneExcelRow.lineId, oneExcelRow.weekToken, emptyArray())
-        oneWeek.dayArray = createAWeekScheduleData(oneExcelRow.weekToken, rowsForThisWeek)
 
-        //oneWeek.dayArray = getDayArrayForThisWeek(oneExcelRow.weekToken, oneExcelRow.dayToken, file)
-        fullScheduleData += oneWeek
-    }
 }
 
-fun createAWeekScheduleData(weekToken: String, rowForThisWeek: Array<ExcelRowData>): Array<aDayScheculeData> {
-    var aDayArray: Array<aDayScheculeData> = emptyArray()
-    var aSectionArray: Array<aSectionScheculeData> = emptyArray()
-    var aActivityArray: Array<aActivityData> = emptyArray()
-    var aDay: aDayScheculeData = aDayScheculeData(currentLine, weekToken,  "", "", "", emptyArray())
-    var aSection: aSectionScheculeData = aSectionScheculeData(0, "", "", "", emptyArray())
-    var aActivity: aActivityData = aActivityData(0, "", "", "", "", "", "", "")
-    var aWeekScheduleData: aWeekScheduleData = aWeekScheduleData(0, "", emptyArray())
-
-    var currentDayToken = ""
-    var currentSectionToken = ""
-    var currentActivityToken = ""
-
-    rowForThisWeek.forEach {
-        aRow
-        println(aRow)
-        currentLine++
-
-        if (aRow.dayToken != "") {
-            currentDayToken = aRow.dayToken
-            aDay = aDayScheculeData(
-                aRow.lineId,
-                aRow.weekToken,
-                aRow.dayToken,
-                aRow.descriptionToken,
-                emptyArray()
-            )
-            aDayArray += aDay
-        } else {
-            if (aRow.sectionToken != "") {
-                currentSectionToken = aRow.sectionToken
-                aSection = aSectionScheculeData(
-                    aRow.lineId,
-                    aRow.weekToken,
-                    aRow.dayToken,
-                    aRow.sectionToken,
-                    emptyArray()
-                )
-                aSectionArray += aSection
-            } else {
-                if (aRow.activityToken != "") {
-                    currentActivityToken = aRow.activityToken
-                    aActivity = aActivityData(
-                        aRow.lineId,
-                        aRow.weekToken,
-                        aRow.dayToken,
-                        aRow.sectionToken,
-                        aRow.activityToken,
-                        aRow.setsToken,
-                        aRow.repsToken,
-                        aRow.descriptionToken
-                    )
-                    aActivityArray += aActivity
-                }
-            }
+fun readFile() {
+    val file = File(fileNameStr)
+    var lineIdx = 0
+    for (line in file.readLines()) {
+        logger.debug { "Line read : $line" }
+        val excelData = createExcelRowData(lineIdx, line)
+        excelSheet.add(excelData)
+        // While at it let us also create a indexes of weeks
+        if (excelData.weekToken != "") {
+            idxOfWeeks.add(excelData)
         }
+        lineIdx++
     }
-    return aDayArray
-}
-
-// DONE: 2021-08-22 - Create a function that returns an array of ExcelRowData for this week
-fun getExcelRowDataForThisWeek(weekToken: String, excelSheetIterator:  ): Array<ExcelRowData> {
-    logger.info { "START getExcelRowDataForThisWeek \t weekToken: $weekToken"}
-    var excelRowDataForThisWeek: Array<ExcelRowData> = emptyArray()
-    file.forEachLine {aLine
-        //println(it)
-        currentLine++
-        logger.info { " Current line: $currentLine \t aLine: $aLine"}
-
-        val oneExcelRow: ExcelRowData = createExcelRowData(aLine)
-        if (oneExcelRow.weekToken == "") {
-            excelRowDataForThisWeek += oneExcelRow
-        } else {
-            //line counter back one
-            currentLine--
-        }
-    }
-    return excelRowDataForThisWeek
+    logger.debug { "Numbers of lines read from $fileNameStr in to the list is ${excelSheet.size}" }
 }
 
 /**
@@ -132,13 +59,13 @@ fun getExcelRowDataForThisWeek(weekToken: String, excelSheetIterator:  ): Array<
  * @param aLine
  * @return ExcelRowData
  */
-fun createExcelRowData(aLine: String): ExcelRowData {
+fun createExcelRowData(lineIdx : Int, aLine: String): ExcelRowData {
     // Parse the comma separated values and use them to create Excel row data
     // convert a String to an Array using comma as a delimiter
     val aLineArray = aLine.toString().split(",").toTypedArray()
     //println(aLineArray[0])
     var excelRowReturnvalue = ExcelRowData(
-        currentLine,
+        lineIdx,
         aLineArray[0],
         aLineArray[1],
         aLineArray[2],
@@ -151,42 +78,3 @@ fun createExcelRowData(aLine: String): ExcelRowData {
 
     return excelRowReturnvalue
 }
-
-fun readFile() {
-    val file = File(fileNameStr)
-    for (line in file.readLines()) {
-        logger.debug { "Line read : $line"  }
-        excelSheet.add(line)
-    }
-    logger.debug { "Numbers of lines read from $fileNameStr in to the list is ${excelSheet.size}" }
-}
-
-// ******************
-// Parking lot; Can be removed later
-// ******************
-
-//fun getDayArrayForThisWeek(weekToken: String, dayToken: String, file: File): Array<aDayScheculeData> {
-//    var dayArray: Array<aDayScheculeData> = emptyArray()
-//    file.forEachLine {
-//        aLine
-//        //println(it)
-//        println(aLine)
-//        currentLine++
-//
-//        val oneExcelRow: ExcelRowData = createExcelRowData(aLine)
-//        if (oneExcelRow.weekToken == "" && (oneExcelRow.dayToken == "" || oneExcelRow.dayToken == dayToken)) {
-//            var oneDay: aDayScheculeData = aDayScheculeData(
-//                oneExcelRow.lineId,
-//                oneExcelRow.weekToken,
-//                oneExcelRow.dayToken,
-//                oneExcelRow.descriptionToken,
-//                emptyArray()
-//            )
-//            dayArray += oneDay
-//        } else {
-//            //line counter back one
-//            currentLine--
-//        }
-//    }
-//    return dayArray
-//}
--- src/main/kotlin/ScratchPad.kt
+++ src/main/kotlin/ScratchPad.kt
@@ -1,73 +1,110 @@
-import Data.ExcelRowData
-import java.io.File
-
-
-var sampleFileContent = mutableListOf<String>()
-var currentLine = 0
-fun main() {
-
-    readThisFile("src/main/resources/WeekSchedule.csv")
-    println(" sampleFileContent.size: ${sampleFileContent.size} ")
-    // Test for passing Iterator to a function
-
-    val excelSheetIterator = sampleFileContent.listIterator()
-    for ((index, aLine) in excelSheetIterator.withIndex()) {
-        //println("CurrentLine is: $currentLine \t Index: $index \t\t Value is: $aLine")
-        if (index != 0) {
-            currentLine++
-            println("CurrentLine is: $currentLine \t Index: $index \t\t Value is: $aLine")
-            val oneExcelRow: ExcelRowData = createExcelRowData(aLine)
-            val rowsForThisWeek: Array<ExcelRowData> =
-                getExcelRowDataForThisWeek(oneExcelRow.weekToken, excelSheetIterator)
-        }
-    }
-
-}
-
-fun readThisFile(fileName: String) {
-    val file = File(fileName)
-    for (line in file.readLines()) {
-        sampleFileContent.add(line)
-    }
-}
-
-fun getExcelRowDataForThisWeek(weekToken: String, excelSheetIterator: Iterator<String>): Array<ExcelRowData> {
-    //logger.info { "START getExcelRowDataForThisWeek \t weekToken: $weekToken"}
-    var excelRowDataForThisWeek: Array<ExcelRowData> = emptyArray()
-    //file.forEachLine {aLine
-    for ((index, aLine) in excelSheetIterator.withIndex()) {
-        //println(it)
-        currentLine++
-        //logger.info { " Current line: $currentLine \t aLine: $aLine"}
-
-        val oneExcelRow: ExcelRowData = createExcelRowData(aLine)
-        if (oneExcelRow.weekToken == "") {
-            excelRowDataForThisWeek += oneExcelRow
-        } else {
-            //line counter back one
-            currentLine--
-            break
-        }
-    }
-    return excelRowDataForThisWeek
-}
-
-fun createExcelRowData(aLine: String): ExcelRowData {
-    // Parse the comma separated values and use them to create Excel row data
-    // convert a String to an Array using comma as a delimiter
-    val aLineArray = aLine.toString().split(",").toTypedArray()
-    //println(aLineArray[0])
-    var excelRowReturnvalue = ExcelRowData(
-        currentLine,
-        aLineArray[0],
-        aLineArray[1],
-        aLineArray[2],
-        aLineArray[3],
-        aLineArray[4],
-        aLineArray[5],
-        aLineArray[6],
-        aLineArray[7]
-    )
-
-    return excelRowReturnvalue
-}
+//import Data.ExcelRowData
+//import java.io.File
+//
+//
+//var sampleFileContent = mutableListOf<String>()
+//var currentLineTemporal = 0
+//fun main() {
+//
+//    readThisFile("src/main/resources/WeekSchedule.csv")
+//    println(" sampleFileContent.size: ${sampleFileContent.size} ")
+//    // Test for passing Iterator to a function
+//
+//    val excelSheetIterator = sampleFileContent.listIterator()
+//    for ((index, aLine) in excelSheetIterator.withIndex()) {
+//        //println("CurrentLine is: $currentLine \t Index: $index \t\t Value is: $aLine")
+//        if (index != 0) {
+//            currentLineTemporal++
+//            println("CurrentLine is: $currentLineTemporal \t Index: $index \t\t Value is: $aLine")
+//            val oneExcelRow: ExcelRowData = createExcelRowData(aLine)
+//            val rowsForThisWeek: Array<ExcelRowData> =
+//                getExcelRowDataForThisWeekTEMP(index, oneExcelRow.weekToken) // , excelSheetIterator)
+//        }
+//    }
+//
+//}
+//
+//fun getExcelRowDataForThisWeekTEMP(weekStartIndex: Int, weekToken: String): Array<ExcelRowData> {
+//    //logger.info { "START getExcelRowDataForThisWeek \t weekToken: $weekToken"}
+//    var excelRowDataForThisWeek: Array<ExcelRowData> = emptyArray()
+//
+//    println("getExcelRowDataForThisWeekTEMP \t weekStartIndex: $weekStartIndex \t weekToken: $weekToken")
+//    println("\t ")
+//    var firstRound = true
+//    for ( idx in weekStartIndex until sampleFileContent.size) {
+//        println("idx: $idx \t sampleFileContent[idx]: ${sampleFileContent[idx]}")
+//        val oneExcelRow: ExcelRowData = createExcelRowData(sampleFileContent[idx])
+//        if ((oneExcelRow.weekToken == "") || (firstRound == true)) {
+//            firstRound = false
+//            excelRowDataForThisWeek += oneExcelRow
+//        } else {
+//            //line counter back one
+//            currentLineTemporal--
+//            break
+//        }
+//    }
+////    for ((index, aLine) in sampleFileContent.withIndex()) {
+////        //println(it)
+////        currentLineTemporal++
+////        //logger.info { " Current line: $currentLine \t aLine: $aLine"}
+////
+////        val oneExcelRow: ExcelRowData = createExcelRowData(aLine)
+////        if (oneExcelRow.weekToken == "") {
+////            excelRowDataForThisWeek += oneExcelRow
+////        } else {
+////            //line counter back one
+////            currentLineTemporal--
+////            break
+////        }
+////    }
+//    println("excelRowDataForThisWeek.size: ${excelRowDataForThisWeek.size}")
+//    return excelRowDataForThisWeek
+//}
+//
+//fun readThisFile(fileName: String) {
+//    val file = File(fileName)
+//    for (line in file.readLines()) {
+//        sampleFileContent.add(line)
+//    }
+//}
+//
+////fun getExcelRowDataForThisWeek(weekToken: String, excelSheetIterator: Iterator<String>): Array<ExcelRowData> {
+////    //logger.info { "START getExcelRowDataForThisWeek \t weekToken: $weekToken"}
+////    var excelRowDataForThisWeek: Array<ExcelRowData> = emptyArray()
+////    //file.forEachLine {aLine
+////    for ((index, aLine) in excelSheetIterator.withIndex()) {
+////        //println(it)
+////        currentLineTemporal++
+////        //logger.info { " Current line: $currentLine \t aLine: $aLine"}
+////
+////        val oneExcelRow: ExcelRowData = createExcelRowData(aLine)
+////        if (oneExcelRow.weekToken == "") {
+////            excelRowDataForThisWeek += oneExcelRow
+////        } else {
+////            //line counter back one
+////            currentLineTemporal--
+////            break
+////        }
+////    }
+////    return excelRowDataForThisWeek
+////}
+////
+//////fun createExcelRowData(aLine: String): ExcelRowData {
+//////    // Parse the comma separated values and use them to create Excel row data
+//////    // convert a String to an Array using comma as a delimiter
+//////    val aLineArray = aLine.toString().split(",").toTypedArray()
+//////    //println(aLineArray[0])
+//////    var excelRowReturnvalue = ExcelRowData(
+//////        currentLineTemporal,
+//////        aLineArray[0],
+//////        aLineArray[1],
+//////        aLineArray[2],
+//////        aLineArray[3],
+//////        aLineArray[4],
+//////        aLineArray[5],
+//////        aLineArray[6],
+//////        aLineArray[7]
+//////    )
+//////
+//////    return excelRowReturnvalue
+//////}
